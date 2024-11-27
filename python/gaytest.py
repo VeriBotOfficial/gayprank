cd7328eaba3bc5fe6364af6613de334d82bb46f0
VeriBotOfficial <142978843+VeriBotOfficial@users.noreply.github.com>
2024-11-27 13:36:03 +0300

    Add files via upload

--- python/gaytest.py
+++ python/gaytest.py
@@ -4,6 +4,10 @@ import random
 def no():
     messagebox.showinfo(' ', 'Gay Spotted')
     quit()
+
+def yes():
+    messagebox.showinfo(' ', 'HACKER OROSPU ÇOCUĞU BACINI SİKİYİM')
+    quit()
     
 def motionMouse(event):
     btnYes.place(x=random.randint(0, 500), y=random.randint(0, 500))
@@ -15,7 +19,7 @@ root.resizable(width=False, height=False)
 root['bg'] = 'white'
 
 Label = Label(root, text='Are You Gay?', font='Arial 20 bold', bg='white').pack()
-btnYes = Button(root, text='No', font='Arial 20 bold')
+btnYes = Button(root, text='No', font='Arial 20 bold',  command=yes)
 btnYes.place(x=170, y=100)
 btnYes.bind('<Enter>', motionMouse)
 btnNo = Button (root, text='Yes', font='Arial 20 bold', command=no).place(x=350, y=100)
